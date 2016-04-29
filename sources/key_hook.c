5c0feb96484630bf01fddb986790604bdb9378b3
SegFault42 <rabougue@student.42.fr>
2016-04-29 11:55:04 +0200

    Ajout Bonus changement vitesse deplacement

--- sources/key_hook.c
+++ sources/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/24 01:05:06 by rabougue          #+#    #+#             */
-/*   Updated: 2016/04/29 09:40:48 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/04/29 11:53:09 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,18 @@ int		mouse_hook(int button, int x, int y, t_all *all)
 
 int		key_hook_m(int keycode, t_all *all)
 {
+	static double speed = 0.1;
+
+	if (keycode == KEY_1)
+		speed = 0.01;
+	if (keycode == KEY_2)
+		speed = 0.05;
+	if (keycode == KEY_3)
+		speed = 0.1;
 	if (keycode == KEY_EQUAL)
 	{
 		clear_image(all);
 		all->zoom *= 1.1;
-		/*all->x2 /= all->x2 + 2;*/
 		draw_mandelbrot(all);
 		mlx_put_image_to_window(all->mlx_ptr, all->win_ptr, all->img_ptr, 0, 0);
 	}
@@ -69,8 +76,6 @@ int		key_hook_m(int keycode, t_all *all)
 	{
 		clear_image(all);
 		all->zoom /= 1.1;
-		/*all->x2 *= 3;*/
-		/*all->y2 = 0;*/
 		draw_mandelbrot(all);
 		mlx_put_image_to_window(all->mlx_ptr, all->win_ptr, all->img_ptr, 0, 0);
 	}
@@ -91,28 +96,28 @@ int		key_hook_m(int keycode, t_all *all)
 	if (keycode == KEY_RIGHT)
 	{
 		clear_image(all);
-		all->x1 *= 1.1;
+		all->x1 -= speed;
 		draw_mandelbrot(all);
 		mlx_put_image_to_window(all->mlx_ptr, all->win_ptr, all->img_ptr, 0, 0);
 	}
 	if (keycode == KEY_LEFT)
 	{
 		clear_image(all);
-		all->x1 /= 1.01;
+		all->x1 += speed;
 		draw_mandelbrot(all);
 		mlx_put_image_to_window(all->mlx_ptr, all->win_ptr, all->img_ptr, 0, 0);
 	}
 	if (keycode == KEY_UP)
 	{
 		clear_image(all);
-		all->y1 /= 1.1;
+		all->y1 += speed;
 		draw_mandelbrot(all);
 		mlx_put_image_to_window(all->mlx_ptr, all->win_ptr, all->img_ptr, 0, 0);
 	}
 	if (keycode == KEY_DOWN)
 	{
 		clear_image(all);
-		all->y1 *= 1.1;
+		all->y1 -= speed;
 		draw_mandelbrot(all);
 		mlx_put_image_to_window(all->mlx_ptr, all->win_ptr, all->img_ptr, 0, 0);
 	}
